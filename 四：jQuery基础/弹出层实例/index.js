598e1f013e5e9403012f62d5f61e9c5105962147
xiaofan <1353815633@qq.com>
2018-03-14 21:42:41 +0800

    jQuery 弹出层案例

--- 四：jQuery基础/弹出层实例/index.js
+++ 四：jQuery基础/弹出层实例/index.js
@@ -2,32 +2,62 @@
 * @Author: xiaofan
 * @Date:   2018-03-14 15:57:11
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-14 17:13:53
+* @Last Modified time: 2018-03-14 21:03:35
 */
 $(document).ready(function ($) {
 	$('#loginLink').click(function () {
-		// $('#layer-mask').show();
-		// $('#layer-pop').show();
-
-		// $('#layer-close').click(function () {
-		// 	$('#layer-mask').hide();
-		// 	$('#layer-pop').hide();
-		// })
-
 		// 获取HTML
 		var loginHtml = $('#loginHtml').html();
+		showLayer(loginHtml,400, 300,closeCallback);
 
-		showLayer(loginHtml);
+		// 登录校验
+		$('#loginSubmitBtn').click(function () {
+			var username = $("input[name = 'username']").val();
+			var password = $("input[name = 'password']").val();
+			if (username == 'xiaofan' && password == 'xiaofan') {
+				alert("登录成功");
+			} else {
+				$(".error-msg").html("账号或密码输入错误");
+			}
+		})
 	})
 
+	$('#regeLink').click(function () {
+		// 获取HTML
+		var regeHtml = $('#regeHtml').html();
+		showLayer(regeHtml,400, 300, closeCallback);
+
+		// 登录校验
+		$('#regeSubmitBtn').click(function () {
+			var password = $("input[name = 'password']").val();
+			var repassword = $("input[name = 'repassword']").val();
+			if (password === repassword) {
+				alert("注册成功");
+			} else {
+				$(".error-msg").html("密码输入不一致");
+			}
+		})
+	})
+
+	function closeCallback() {
+		$(".error-msg").html("");
+	}
+
 	// 打开弹出层
-	function showLayer(html) {
+	function showLayer(html,width,height,closeCallback) {
 		$('#layer-mask').show();
 		$('#layer-pop').show();
+		// jQuery动态设置样式
+		$('#layer-pop').css({
+			width:width,
+			height:height
+		})
 		// 填充登录HTML
 		$('#layer-content').html(html);
 		$('#layer-close').click(function () {
 			hideLayer();
+			// 关闭回调函数
+			closeCallback();
 		})
 	}
 
