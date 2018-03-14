b75fa3c560852e0778b63821de6663e7d6dea368
xiaofan <1353815633@qq.com>
2018-03-14 17:02:57 +0800

    弹窗登录界面

--- 四：jQuery基础/弹出层实例/index.js
+++ 四：jQuery基础/弹出层实例/index.js
@@ -2,5 +2,43 @@
 * @Author: xiaofan
 * @Date:   2018-03-14 15:57:11
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-14 15:57:11
+* @Last Modified time: 2018-03-14 16:43:33
 */
+$(document).ready(function ($) {
+	$('#loginLink').click(function () {
+		$('#layer-mask').show();
+		$('#layer-pop').show();
+
+		$('#layer-close').click(function () {
+			$('#layer-mask').hide();
+			$('#layer-pop').hide();
+		})
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
