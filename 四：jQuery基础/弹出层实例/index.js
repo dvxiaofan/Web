5612e5a0b60008fff48a01c364627ac3b99ad638
xiaofan <1353815633@qq.com>
2018-03-14 17:16:37 +0800

    简单封装登录事件

--- 四：jQuery基础/弹出层实例/index.js
+++ 四：jQuery基础/弹出层实例/index.js
@@ -2,18 +2,40 @@
 * @Author: xiaofan
 * @Date:   2018-03-14 15:57:11
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-14 16:43:33
+* @Last Modified time: 2018-03-14 17:13:53
 */
 $(document).ready(function ($) {
 	$('#loginLink').click(function () {
+		// $('#layer-mask').show();
+		// $('#layer-pop').show();
+
+		// $('#layer-close').click(function () {
+		// 	$('#layer-mask').hide();
+		// 	$('#layer-pop').hide();
+		// })
+
+		// 获取HTML
+		var loginHtml = $('#loginHtml').html();
+
+		showLayer(loginHtml);
+	})
+
+	// 打开弹出层
+	function showLayer(html) {
 		$('#layer-mask').show();
 		$('#layer-pop').show();
-
+		// 填充登录HTML
+		$('#layer-content').html(html);
 		$('#layer-close').click(function () {
-			$('#layer-mask').hide();
-			$('#layer-pop').hide();
+			hideLayer();
 		})
-	})
+	}
+
+	// 关闭弹出层
+	function hideLayer() {
+		$('#layer-mask').hide();
+		$('#layer-pop').hide();
+	}
 })
 
 
