e31ca4c61b7494fe9bc65cf9863eafa1d43a40ae
xiaofan <1353815633@qq.com>
2018-03-15 17:37:45 +0800

    完成瀑布流布局

--- 四：jQuery基础/瀑布流/js/index.js
+++ 四：jQuery基础/瀑布流/js/index.js
@@ -2,7 +2,7 @@
 * @Author: xiaofan
 * @Date:   2018-03-15 10:31:35
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-15 15:03:09
+* @Last Modified time: 2018-03-15 17:31:16
 */
 
 // function waterfall(wrap, boxes) {
@@ -31,6 +31,63 @@
 // 	}
 // }
 
+var data = [
+{
+	"src" : "bg001.png",
+	"title": "江南十八怪"
+} ,{
+	"src" : "bg002.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg003.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg004.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg005.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg006.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg007.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg008.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg009.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg010.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg011.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg012.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg013.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg014.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg015.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg016.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg017.png",
+	"title": "江南十八怪"
+},{
+	"src" : "bg018.png",
+	"title": "江南十八怪"
+}];
+
 var waterfall = function (wrap, boxes) {
 	var boxWidth = boxes.eq(0).width() + 40;
 	var windowWidth = $(window).width();
@@ -38,6 +95,7 @@ var waterfall = function (wrap, boxes) {
 
 	wrap.width(boxWidth * colsNum);
 
+	// 定义一个数组并保存每一列高度
 	var everyHeight = new Array();
 
 	for (var i = 0; i < boxes.length; i++) {
@@ -47,14 +105,29 @@ var waterfall = function (wrap, boxes) {
 			var minHeight = Math.min.apply(null, everyHeight);
 			var minIndex = getIndex(minHeight, everyHeight);
 
-			boxes.eq(i).css({
-				'position': "absolute",
-				'left': boxes.eq(minIndex).position().left,
-				'top': minHeight
-			});
+			setStyle(boxes.eq(i), minHeight, boxes.eq(minIndex).position().left, i)
+
+			// boxes.eq(i).css({
+			// 	'position': "absolute",
+			// 	'left': boxes.eq(minIndex).position().left,
+			// 	'top': minHeight,
+			// 	'opacity': '0'
+			// }).stop().animate({
+			// 	'opacity' : '1'
+			// }, 1000);
 
 			everyHeight[minIndex] += boxes.eq(i).height() + 40;
 		}
+
+		boxes.eq(i).hover(function (event) {
+			$(this).stop().animate({
+				'opacity' : '0.5'
+			}, 500);
+		}, function (event) {
+			$(this).stop().animate({
+				'opacity' : '1'
+			}, 500);
+		})
 	}
 }
 
@@ -73,10 +146,58 @@ function getIndex(minHeight, everyHeight) {
 // 	waterfall(wrap, boxes);
 // }
 
+function appendBox(wrap, boxes) {
+	if (getCheck(wrap)) {
+		for (i in data) {
+			var innerStr = '<div><img src="../img/' + data[i].src + '"><a href="https://www.baidu.com/" target="_blank">' + data[i].title + '</a></div>';
+			wrap.append(innerStr);
+		} 
+	} else {
+		return false;
+	}
+	waterfall(wrap, wrap.children('div'));
+}
+
+// 设置追加盒子样式
+var getStartNum = 0;
+function setStyle(box, top, left, index) {
+	if (getStartNum >= index) {
+		return false;
+	}
+	box.css({
+		'position': "absolute",
+		'left': left,
+		'top': top,
+		'opacity': '0'
+	}).stop().animate({
+				'opacity' : '1'
+	}, 1000);
+	getStartNum = index;
+}
+
+// 数据请求检验
+function getCheck(wrap) {
+	var documentHeight = $(window).height();
+	// 获取文档向上滚动的高度
+	var scrollHeight = $(window).scrollTop();
+	// 获取最后一个盒子所在列的总高度
+	var boxes = wrap.children('div');
+	var lastBoxTop = boxes.eq(boxes.length - 1).offset().top;
+	var lastHeight = boxes.eq(boxes.length - 1).height() + 20;
+
+	var lastColHeight = lastBoxTop + lastHeight;
+
+	return documentHeight + scrollHeight >= lastColHeight ? true : false;
+}
+
 $(document).ready(function(event){
 	var wrap = $('#wrap');
 	var boxes = $('#wrap').children('div');
 	waterfall(wrap, boxes);
+
+	$(this).scroll(function (event) {
+		appendBox(wrap, boxes);
+	})
 })
 
 
