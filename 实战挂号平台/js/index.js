83e7b483ec6b40e683de4e8ffbc971aa07d5dc48
xiaofan <1353815633@qq.com>
2018-03-16 17:13:04 +0800

    创建挂号平台项目

---
+++ 实战挂号平台/js/index.js
@@ -0,0 +1,31 @@
+/*
+* @Author: xiaofan
+* @Date:   2018-03-16 17:02:26
+* @Last Modified by:   xiaofan
+* @Last Modified time: 2018-03-16 17:11:00
+*/
+
+
+$('#demo').click(function (event) {
+	console.log("test");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
