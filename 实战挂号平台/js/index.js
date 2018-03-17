84a92219380a0fec5fdb4f0312597cba98a9b9b0
xiaofan <1353815633@qq.com>
2018-03-17 22:00:38 +0800

    031702

--- 实战挂号平台/js/index.js
+++ 实战挂号平台/js/index.js
@@ -2,14 +2,21 @@
 * @Author: xiaofan
 * @Date:   2018-03-16 17:02:26
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-16 17:11:00
+* @Last Modified time: 2018-03-17 22:00:27
 */
 
 
-$('#demo').click(function (event) {
-	console.log("test");
+$('#nav').click(function (event) {
+	console.log(this);
 })
 
+$('#banner').click(function (event) {
+	console.log(this);
+})
+
+$('#foot').click(function (event) {
+	console.log(this);
+})
 
 
 
