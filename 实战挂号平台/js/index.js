72a0f5968e0315e39fe5b5a9d6fbc261c65d9111
xiaofan <1353815633@qq.com>
2018-03-19 15:58:24 +0800

    banner

--- 实战挂号平台/js/index.js
+++ 实战挂号平台/js/index.js
@@ -2,7 +2,7 @@
 * @Author: xiaofan
 * @Date:   2018-03-16 17:02:26
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-17 22:00:27
+* @Last Modified time: 2018-03-19 15:06:14
 */
 
 
@@ -14,7 +14,7 @@ $('#banner').click(function (event) {
 	console.log(this);
 })
 
-$('#foot').click(function (event) {
+$('#footer').click(function (event) {
 	console.log(this);
 })
 
