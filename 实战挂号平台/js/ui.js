7cf5ed501d00d663054fcd4e48e9fe47e98a3662
xiaofan <1353815633@qq.com>
2018-03-20 16:56:33 +0800

    UI-search

---
+++ 实战挂号平台/js/ui.js
@@ -0,0 +1,39 @@
+/*
+* @Author: xiaofan
+* @Date:   2018-03-20 16:14:13
+* @Last Modified by:   xiaofan
+* @Last Modified time: 2018-03-20 16:53:43
+*/
+
+$('#ui-search-submit').click(function (event) {
+	alert("ui-search-submit")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
