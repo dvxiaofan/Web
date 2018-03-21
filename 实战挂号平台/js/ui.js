84de47b674290c50903ddd3234b6a5455ebb79c8
xiaofan <1353815633@qq.com>
2018-03-21 14:40:36 +0800

    uisearch

--- 实战挂号平台/js/ui.js
+++ 实战挂号平台/js/ui.js
@@ -2,14 +2,31 @@
 * @Author: xiaofan
 * @Date:   2018-03-20 16:14:13
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-20 16:53:43
+* @Last Modified time: 2018-03-21 12:37:07
 */
 
-$('#ui-search-submit').click(function (event) {
-	alert("ui-search-submit")
+// ui-search 定义
+$.fn.Uisearch = function () {
+	var ui = (this);
+
+	$('.ui-search-selected', ui).on('click', function () {
+		$('.ui-search-selected-list').show();
+
+		return false;
+	});
+
+	$('.ui-search-selected-list a', ui).on('click', function () {
+		$('.ui-search-selected').text($(this).text());
+		$('.ui-search-selected-list').hide();
+
+		return false;
+	})
+}
+
+// 页面脚本逻辑
+$(function () {
+	$('.ui-search').Uisearch();
 })
-
-
 
 
 
