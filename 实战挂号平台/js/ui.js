1163b7d6960bb16ab2bbddfa43aed7b43eb5eac7
xiaofan <1353815633@qq.com>
2018-03-22 17:08:13 +0800

    UiSlider实现点击切换

--- 实战挂号平台/js/ui.js
+++ 实战挂号平台/js/ui.js
@@ -2,7 +2,7 @@
 * @Author: xiaofan
 * @Date:   2018-03-20 16:14:13
 * @Last Modified by:   xiaofan
-* @Last Modified time: 2018-03-21 17:17:37
+* @Last Modified time: 2018-03-22 17:06:56
 */
 
 // ui-search 定义
@@ -47,12 +47,90 @@ $.fn.UiTab = function (header, content, focus_prefix) {
 	})
 }
 
+// 返回顶部
+$.fn.UiBackTop = function () {
+	var ui = $(this);
+	var el = $('<a href="#" class="ui-backTop"></a>');
+	ui.append(el);
+
+	var winHeight = $(window).height();
+	$(window).on('scroll', function () {
+		var top = $(window).scrollTop();
+		if (top > winHeight) {
+			el.show();
+		} else {
+			el.hide();
+		}
+	});
+
+	el.on('click', function () {
+		$(window).scrollTop(0);
+	})
+}
+
+// ui-slider操作
+
+// 1，左右箭头需要能控制页面滚动
+// 2，翻页的时候， 进度点要联动进行focus
+// 3，翻到第三页的时候， 下一页要回到第一页， 翻到第一页的时候，同理
+
+// 4，进度点， 点击的时候需要跳转的对应页面
+
+// 5，没有（进度点点击、翻页操作）的时候需要自动滚动
+
+// 6，滚动的过程中，屏蔽其他操作（翻页操作、进度点点击、自动滚动）
+
+$.fn.UiSlider = function () {
+	var ui = $(this);
+	var wrap = $('.ui-slider-wrap');
+
+	var btn_prev = $('.ui-slider-arrow .left', ui);
+	var btn_next = $('.ui-slider-arrow .right', ui);
+
+	var items = $('.ui-slider-wrap .item', ui);
+	var tips = $('.ui-slider-process .item', ui);
+
+	// 预定义
+	var current = 0;
+	var size = items.size();
+	var width = items.eq(0).width();
+
+	// 具体操作
+	wrap
+	.on('move_prev',function () {
+		console.log("prev")
+	})
+	.on('move_next',function () {
+		console.log("next")
+	})
+	.on('move_to',function (evt, index) {
+		wrap.css('left', index*width*-1);
+	})
+
+	// 事件
+	btn_prev.on('click', function () {
+		wrap.triggerHandler('move_prev');
+	})
+
+	btn_next.on('click', function () {
+		wrap.triggerHandler('move_next');
+	})
+	tips.on('click',function () {
+		var index = $(this).index();
+		wrap.triggerHandler('move_to', index);
+	})
+}
+
 // 页面脚本逻辑
 $(function () {
 	$('.ui-search').Uisearch();
 
 	$('.content-tab').UiTab('.caption > .item', '.block > .item');
 	$('.content-tab .block .item').UiTab('.block-caption > a', '.block-content > .block-wrap', 'block_');
+
+	$('body').UiBackTop();
+
+	$('.ui-slider').UiSlider();
 })
 
 
