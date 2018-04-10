50060ccb43061d6ae4c6f4d6f673979b765cfcd1
xiaofan <1353815633@qq.com>
2018-04-10 18:43:47 +0800

    udacity

---
+++ Udacity/JS基础/frontend-nanodegree-resume/js/resumeBuilder.js
@@ -0,0 +1,52 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+ // $("#main").append("xiaofan");
+ // $("#main").append("xiaoming");
+
+ // var email = "zhyk1023@163.com";
+ // var newEmail = email.replace("163", "126");
+
+ // console.log(newEmail);
+
+ // var awesomeThoughts = "My name is xiaofan and I am AWESOME";
+ // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+ // $("#main").append(funThoughts);
+
+var name = "张艳坤"
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var role = "Web前端开发工程师";
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ // $("#header").append(formattedName);
+ // $("#header").append(formattedRole);
+
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
